af2c098bd71390e90660cc250614e74218b46e17
MSRESupport <150456773+MSRESupport@users.noreply.github.com>
2024-12-29 13:52:47 -0500

    Update delDirectory.ps1

--- delDirectory.ps1
+++ delDirectory.ps1
@@ -1,6 +1,9 @@
 # Define the registry path and the key to remove
 $registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AAD\Storage"
 $keyToRemove = "https://login.microsoftonline.com"
+
+$identityRegistryPath = "HKCU:Software\Microsoft\Office\16.0\Common\Identity"
+$identityKeyToRemove = "Identities"
  
 # Check if the registry path exists
 if (Test-Path $registryPath) {
@@ -15,6 +18,21 @@ if (Test-Path $registryPath) {
     }
 } else {
     Write-Host "The registry path '$registryPath' does not exist."
+}
+
+# Check if the registry path exists
+if (Test-Path $identityRegistryPath) {
+    # Check if the specific key exists
+    if (Test-Path "$identityRegistryPath\$identityKeyToRemove") {
+        # Remove the key
+        Write-Host 'Attempting to remove $identityRegistryPath'
+        Remove-Item "$identityRegistryPath\$identityKeyToRemove" -Force
+        Write-Host "Registry entry '$identityKeyToRemove' removed successfully."
+    } else {
+        Write-Host "The key '$identityKeyToRemove' does not exist."
+    }
+} else {
+    Write-Host "The registry path '$identityRegistryPath' does not exist."
 }
 
 Write-Host 'Creating registry files'
