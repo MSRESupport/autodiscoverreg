996c67386599b5f13dbbf0ca036f6d8d72ac690d
MSRESupport <150456773+MSRESupport@users.noreply.github.com>
2024-12-29 11:15:19 -0500

    Update delDirectory.ps1

--- delDirectory.ps1
+++ delDirectory.ps1
@@ -1,13 +1,19 @@
-# Define the registry path
-$registryPath = "HKCU:\SOFTWARE\Windows\CurrentVersion\AAD\Storage\https://login.microsoftonline.com"
+# Define the registry path and the key to remove
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AAD\Storage"
+$keyToRemove = "https://login.microsoftonline.com"
  
-# Check if the registry key exists
-if (Test-Path -Path $registryPath) {
-    # Delete the registry key
-    Remove-Item -Path $registryPath -Recurse -Force
-    Write-Host "Registry key deleted successfully."
+# Check if the registry path exists
+if (Test-Path $registryPath) {
+    # Check if the specific key exists
+    if (Test-Path "$registryPath\$keyToRemove") {
+        # Remove the key
+        Remove-Item "$registryPath\$keyToRemove" -Force
+        Write-Host "Registry entry '$keyToRemove' removed successfully."
+    } else {
+        Write-Host "The key '$keyToRemove' does not exist."
+    }
 } else {
-    Write-Host "Registry key does not exist."
+    Write-Host "The registry path '$registryPath' does not exist."
 }
 
 Write-Host 'Creating registry files'
